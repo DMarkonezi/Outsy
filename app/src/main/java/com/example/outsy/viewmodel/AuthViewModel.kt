ab8a657d488296db538d2378caa5c1883ba82982
Markonezi <dusanmarkovic1021@gmail.com>
2025-11-13 16:51:48 +0100

    Created functions for different user types registration and added states that manage the UI

--- app/src/main/java/com/example/outsy/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/outsy/viewmodel/AuthViewModel.kt
@@ -6,26 +6,34 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.outsy.data.repository.AuthRepository
 import com.google.firebase.firestore.GeoPoint
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class AuthViewModel(
     private val repository: AuthRepository = AuthRepository()
 ) : ViewModel() {
+    // States
+    private val _authState = MutableStateFlow<Pair<Boolean, String?>>(Pair(false, null))
+    val authState: StateFlow<Pair<Boolean, String?>> = _authState.asStateFlow()
 
-    private val _authState = MutableLiveData<Pair<Boolean, String?>>()
-    val authState: LiveData<Pair<Boolean, String?>> = _authState
+    private val _isLoading = MutableStateFlow(false)
+    val isLoading: StateFlow<Boolean> = _isLoading.asStateFlow()
 
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> = _isLoading
+    private val _userType = MutableStateFlow<String?>(null)
+    val userType: StateFlow<String?> = _userType.asStateFlow()
 
     fun login(email: String, password: String) {
         _isLoading.value = true
-        repository.login(email, password) { success, error ->
+        repository.login(email, password) { success, error, type ->
             _authState.value = Pair(success, error)
             _isLoading.value = false
+            _userType.value = type
         }
     }
 
-    fun register(
+    fun registerUser(
+        username: String,
         firstName: String,
         lastName: String,
         email: String,
@@ -36,11 +44,24 @@ class AuthViewModel(
         onResult: (Boolean, String?) -> Unit
     ) {
         _isLoading.value = true
+        repository.registerUser(username, firstName, lastName, email, password, phoneNumber, location, profileBitmap, onResult)
+    }
 
-        repository.register(firstName, lastName, email, password, phoneNumber, location, profileBitmap, onResult)
+    fun registerPlaceOwner(
+        businessName: String,
+        email: String,
+        password: String,
+        phoneNumber: String,
+        onResult: (Boolean, String?) -> Unit
+    )
+    {
+        _isLoading.value = true;
+        repository.registerPlaceOwner(businessName, email, password, phoneNumber, onResult);
     }
 
     fun logout() {
         repository.logout()
+        _userType.value = null
+        _authState.value = Pair(false, null)
     }
 }
