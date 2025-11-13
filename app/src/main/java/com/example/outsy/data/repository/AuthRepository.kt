17d8e4082e99b0fada2fd9f54b2d0a2fd80116e8
Markonezi <dusanmarkovic1021@gmail.com>
2025-11-13 16:53:26 +0100

    Updated auth repo for made changes in user type login and registration logic

--- app/src/main/java/com/example/outsy/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/outsy/data/repository/AuthRepository.kt
@@ -2,6 +2,7 @@ package com.example.outsy.data.repository
 
 import android.graphics.Bitmap
 import android.util.Log
+import com.example.outsy.data.models.PlaceOwner
 import com.example.outsy.data.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -15,15 +16,33 @@ class AuthRepository(
     private val storage: FirebaseStorage = FirebaseStorage.getInstance()
 ) {
 
-    fun login(email: String, password: String, onResult: (Boolean, String?) -> Unit) {
+    fun login(email: String, password: String, onResult: (Boolean, String?, String?) -> Unit) {
         auth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) onResult(true, null)
-                else onResult(false, task.exception?.message)
+            .addOnSuccessListener { result ->
+                val userId = result.user?.uid
+                if (userId != null) {
+                    firestore.collection("placeOwners").document(userId).get()
+                        .addOnSuccessListener { doc ->
+                            if (doc.exists()) {
+                                onResult(true, null, "placeowner")
+                            } else {
+                                onResult(true, null, "user")
+                            }
+                        }
+                        .addOnFailureListener {
+                            onResult(true, null, "user")
+                        }
+                } else {
+                    onResult(false, "User not found", null)
+                }
+            }
+            .addOnFailureListener { e ->
+                onResult(false, e.message, null)
             }
     }
 
-    fun register(
+    fun registerUser(
+        username: String,
         firstName: String,
         lastName: String,
         email: String,
@@ -54,11 +73,41 @@ class AuthRepository(
 
                 if (profileBitmap != null) {
                     uploadProfileImage(uid, profileBitmap) { imageUrl ->
-                        saveUserToFirestore(uid, firstName, lastName, email, phoneNumber, location, imageUrl, onResult)
+                        saveUserToFirestore(uid, username, firstName, lastName, email, phoneNumber, location, imageUrl, onResult)
                     }
                 } else {
-                    saveUserToFirestore(uid, firstName, lastName, email, phoneNumber, location, null, onResult)
+                    saveUserToFirestore(uid, username, firstName, lastName, email, phoneNumber, location, null, onResult)
                 }
+            }
+    }
+
+    fun registerPlaceOwner(
+        businessName: String,
+        email: String,
+        password: String,
+        phoneNumber: String,
+        onResult: (Boolean, String?) -> Unit
+    )
+    {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.e("Register Error - Place Owner", "Auth failed: ${task.exception?.message}")
+                    onResult(false, task.exception?.message)
+                    return@addOnCompleteListener
+                }
+
+                val uid = auth.currentUser?.uid
+
+                if (uid == null) {
+                    Log.e("Register Error - Place Owner", "UID is null")
+                    onResult(false, "UID is null")
+                    return@addOnCompleteListener
+                }
+
+                Log.d("Register - Place Owner", "Creating user document for UID: $uid")
+
+                savePlaceOwnerToFirestore(uid, businessName, email, phoneNumber, onResult);
             }
     }
 
@@ -84,6 +133,7 @@ class AuthRepository(
 
     private fun saveUserToFirestore(
         uid: String,
+        username: String,
         firstName: String,
         lastName: String,
         email: String,
@@ -94,6 +144,7 @@ class AuthRepository(
     ) {
         val user = User(
             uid = uid,
+            username = username,
             firstName = firstName,
             lastName = lastName,
             email = email,
@@ -108,16 +159,42 @@ class AuthRepository(
             .document(uid)
             .set(user)
             .addOnSuccessListener {
-                Log.d("Register", "User saved successfully")
+                Log.d("Register - User", "User saved successfully")
                 onResult(true, null)
             }
             .addOnFailureListener { e ->
-                Log.e("Register", "Firestore write failed: ${e.message}")
+                Log.e("Register - User", "Firestore write failed: ${e.message}")
                 onResult(false, e.message)
             }
     }
 
-    // TODO dodati neki email check da li je validan i slicno
+    private fun savePlaceOwnerToFirestore(
+        id: String,
+        businessName: String,
+        email: String,
+        phoneNumber: String,
+        onResult: (Boolean, String?) -> Unit
+    )
+    {
+        val placeOwner = PlaceOwner(
+            id = id,
+            businessName = businessName,
+            email = email,
+            phoneNumber = phoneNumber
+        )
+
+        firestore.collection("placeOwners")
+            .document(id)
+            .set(placeOwner)
+            .addOnSuccessListener {
+                Log.d("Register - Place Owner", "Place Owner saved successfully")
+                onResult(true, null)
+            }
+            .addOnFailureListener { e ->
+                Log.e("Register - Place Owner", "Firestore write failed: ${e.message}")
+                onResult(false, e.message)
+            }
+    }
 
     fun logout() {
         auth.signOut()
